15b36c984dbc694441afab71adc8624172d1de7a
moriwakihikari <daiichidaimandaikichi.gi@gmail.com>
2022-02-21 01:23:04 +0900

    大文字を小文字にauth

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // env.に設定してる NEXT_PUBLIC_ process.envで呼び出せる
 // import Image from "next/image";
 import Layout from "../components/Layout";
-import Auth from "../components/auth";
+import Auth from "../components/Auth";
 
 export default function Home() {
   console.log(process.env.NEXT_PUBLIC_RESTAPI_URL);
